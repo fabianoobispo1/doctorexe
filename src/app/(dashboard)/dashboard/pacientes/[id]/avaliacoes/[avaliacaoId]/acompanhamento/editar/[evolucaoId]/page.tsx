0560d8d9b56fd59ba8b5d60b22030b673eef8590
fabianoobispo1 <fabianoobispo@icloud.com>
2025-03-26 16:53:08 -0300

    correcao parametros

--- src/app/(dashboard)/dashboard/pacientes/[id]/avaliacoes/[avaliacaoId]/acompanhamento/editar/[evolucaoId]/page.tsx
+++ src/app/(dashboard)/dashboard/pacientes/[id]/avaliacoes/[avaliacaoId]/acompanhamento/editar/[evolucaoId]/page.tsx
@@ -1,17 +1,20 @@
 import { AcompanhamnetoForm } from '../../nova/acompanhamento-form'
 
-export default function EditarEvolucaoPage({
+export default async function EditarEvolucaoPage({
   params,
 }: {
-  params: { id: string; avaliacaoId: string; evolucaoId: string }
+  params: Promise<{ id: string; avaliacaoId: string; evolucaoId: string }>
 }) {
+  const id = (await params).id
+  const avaliacaoId = (await params).avaliacaoId
+  const evolucaoId = (await params).evolucaoId
   return (
     <div className="flex flex-col gap-4 p-6">
       <h1 className="text-3xl font-bold tracking-tight">Editar Consulta</h1>
       <AcompanhamnetoForm
-        idPaciente={params.id}
-        idAvaliacao={params.avaliacaoId}
-        evolucaoId={params.evolucaoId}
+        idPaciente={id}
+        idAvaliacao={avaliacaoId}
+        evolucaoId={evolucaoId}
         isEditing={true}
       />
     </div>
